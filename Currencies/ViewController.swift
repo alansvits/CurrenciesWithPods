323b8249e9147b10de5a41c3b35662c5115fd03d
Stas Shetko <alansvits@Stass-MacBook-Pro.local>
2018-11-08 19:31:50 +0200

    Initial Commit

---
+++ Currencies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Currencies
+//
+//  Created by Stas Shetko on 2/11/18.
+//  Copyright © 2018 Stas Shetko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
