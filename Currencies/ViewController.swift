9cb77e7c2473bd6fa040f761ee7fb9a81e89b09f
Stas Shetko <alansvits@Stass-MacBook-Pro.local>
2018-11-09 21:52:41 +0200

    Add popover basic.

--- Currencies/ViewController.swift
+++ Currencies/ViewController.swift
@@ -8,12 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPopoverPresentationControllerDelegate {
 
+    @IBAction func showDatePicker(_ sender: Any) {
+
+        performSegue(withIdentifier: "popOver", sender: self)
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "popOver" {
+            let destinationVC = segue.destination
+            if let popoverVC = destinationVC.popoverPresentationController {
+                popoverVC.delegate = self
+            }
+        }
+    }
+    
+    func adaptivePresentationStyle(for controller: UIPresentationController) -> UIModalPresentationStyle {
+        return .none
     }
     
     override var prefersStatusBarHidden: Bool {
